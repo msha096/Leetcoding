40e8e8cb1d0acd6aa2355e5aa8080f001a8377f9
msha096 <msha096@uottawa.ca>
2020-07-31 01:04:28 -0400

    update

--- July-challenge/Word Break II.py
+++ July-challenge/Word Break II.py
@@ -6,6 +6,7 @@ class Solution(object):
         :rtype: List[str]
         """
         # DP algo, before generate the results, we first verify the string by helper()
+        ''' # DP algo is 98% faster
         dp = [[False,[]] for i in range(len(s)+1)]
         dp[0][0] = True
         word = set(wordDict)
@@ -33,4 +34,24 @@ class Solution(object):
                     else:
                         for every in dp[i][1]:
                             dp[i+len(w)][1].append(every+' '+ w)
-        return dp[-1][1]
+        return dp[-1][1]'''
+
+        # recursive and provide a more efficient validation method by comparing set
+        # 86% faster
+        word = set(wordDict)
+        d = set(''.join(wordDict)) #concatenate into a string and split by using set
+        st = set(s)
+        
+        if st - d: # if st contains some char that not exist in the word dict
+            return [] # quick verify
+        res = []
+        n = len(s)
+        def dfs(ans = [], ind = 0):
+            if ind == n:
+                res.append(' '.join(ans))
+                return
+            for i in range(ind,n+1):
+                if s[ind:i] in word:
+                    dfs(ans+[s[ind:i]],i)
+        dfs()
+        return res
